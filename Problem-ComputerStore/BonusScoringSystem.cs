ee55b97f3979ef3d098b31c37a7d95d73a0f71c0
R.Hristova <145340388+RadoslavaHristova@users.noreply.github.com>
2023-10-20 16:48:16 +0100

    Add files via upload

---
+++ Problem-ComputerStore/BonusScoringSystem.cs
@@ -0,0 +1,55 @@
+﻿/*
+5
+25
+30
+12
+19
+24
+16
+20
+
+10
+30
+14
+8
+23
+27
+28
+15
+17
+25
+26
+5
+18
+
+ */
+
+namespace Problem_BonusScoringSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int studentsQty=int.Parse(Console.ReadLine());
+            int lecturesQty=int.Parse(Console.ReadLine());
+            int additionalBonus=int.Parse(Console.ReadLine());
+            double maxBonus = 0;
+            int maxAttended = 0;
+            for(int i=0; i<studentsQty; i++) 
+            {
+            int attendancePerStudent=int.Parse(Console.ReadLine());
+                double totalBonus= 1.0*attendancePerStudent / lecturesQty * (5 + additionalBonus);
+                
+                if(totalBonus>maxBonus) 
+                {
+                    maxBonus= totalBonus;
+                    maxAttended= attendancePerStudent;
+                }
+
+            }
+            Console.WriteLine($"Max Bonus: {Math.Ceiling(maxBonus)}.");
+            Console.WriteLine($"The student has attended {maxAttended} lectures.");
+
+        }
+    }
+}
